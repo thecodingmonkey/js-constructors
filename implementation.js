c179056cacd1561842fbca5411121fda91288919
Andrew Wong <coding.monkey.andrew@gmail.com>
2015-01-30 18:56:02 -1000

    clean up code

--- implementation.js
+++ implementation.js
@@ -1,11 +1,11 @@
 var wizards = new Array(6);
 
-wizards[0] = elsa = new Spellcaster('Elsa', 9999, 100);
-wizards[1] = tahlkora = new Spellcaster('Tahlkora', 9999, 100);
-wizards[2] = aeris = new Spellcaster('Aeris', 9999, 100);
-wizards[3] = vivi = new FireSpellcaster('Vivi', 9999, 100);
-wizards[4] = terra = new FireSpellcaster('Terra', 9999, 100);
-wizards[5] = vekk = new FireSpellcaster('Vekk', 9999, 100);
+wizards[0] = elsa = new Spellcaster('Elsa', 9999, 400);
+wizards[1] = tahlkora = new Spellcaster('Tahlkora', 9999, 400);
+wizards[2] = aeris = new Spellcaster('Aeris', 9999, 400);
+wizards[3] = vivi = new FireSpellcaster('Vivi', 9999, 400);
+wizards[4] = terra = new FireSpellcaster('Terra', 9999, 400);
+wizards[5] = vekk = new FireSpellcaster('Vekk', 9999, 400);
 
 var spellbook = [];
 spellbook.push( new DamageSpell('INVALID SPELL', 99999, 0, 'Select a new spell, plox.'));
@@ -29,6 +29,8 @@ function logEntry(owner, action, target) {
 
   this.message = owner.name + " casts " + action.name + " on " + target.name +
    " for " + action.damage + " damage.";
+
+  console.log(this.message);
 }
 
 
@@ -39,30 +41,26 @@ while (wizards.length > 1) {
   var i;
   for (i in wizards) {
     if (wizards[i].health > 0) {
-      console.log('It is now', wizards[i].name + "'s", ' turn.');
-
+      // pick a spell.  its possible the wizard might choose a spell they don't
+      // have enough mana for; however, FireSpellcasters should be allowed to
+      // pick a spell if they can cast it.
       var j = 0;
       while (spellbook[j].cost > (wizards[i].mana * 2)) {
         j = Math.floor( Math.random() * spellbook.length);
       }
-      console.log('Spell selected:', spellbook[j].name);
 
+      // pick a target at random, but should not pick self
       var target = i;
       while (target == i) {
         target = Math.floor( Math.random() * wizards.length);
       }
-      console.log('Target selected:', target);
 
-      wizards[i].invoke(spellbook[j], wizards[target]);
-      console.log(wizards[i].name, "deals", spellbook[j].damage, "damage to", wizards[target].name, wizards[target].health);
-
-      combatLog.push(
-          new logEntry(wizards[i], spellbook[j], wizards[target])
-        );
-
-//      setTimeout()
-      wizards.filter(function(x) {return x.isAlive;});
-
+      // cast the spell, and push into combat log
+      if (wizards[i].invoke(spellbook[j], wizards[target]) ) {
+        combatLog.push(
+            new logEntry(wizards[i], spellbook[j], wizards[target])
+          );
+      }
     }
   }
   wizards = wizards.filter(function(x) {return x.isAlive;});
